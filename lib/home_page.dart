0bfeafb6107fd588635b5554f97423285e9e0dc2
Benedict Osei Sefa <benedictoseisefa@Benedicts-MacBook-Pro.local>
2022-07-16 08:55:41 +0000

    added colors.dart, in which i placed all the colors in a class by importing dart:ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,6 +12,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      body: Center(
+        child: Text("home page"),
+      ),
+    );
   }
 }
